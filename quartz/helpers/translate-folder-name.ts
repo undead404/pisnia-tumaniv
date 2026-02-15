3316863a4d243c0f9f23036e4f42850e2511a92e
Vitalii Perehonchuk <vitalii_perehonchuk@epam.com>
2026-02-15 22:34:45 +0200

    vault backup: 2026-02-15 22:34:45

--- quartz/helpers/translate-folder-name.ts
+++ quartz/helpers/translate-folder-name.ts
@@ -1,21 +1,22 @@
 export default function translateFolderName(folderName: string) {
-    const nameMap: Record<string, string> = {
-        "cataphract": "⚔️ Катафракти",
-        "celestial": "🪐 Небесні тіла",
-        "episodes": "🎬 Епізоди",
-        "faction": "🚩 Фракції",
-        "location": "📍 Місця",
-        "npc": "👤 НІПи",
-        "pc": "👤 ІПи",
-        "region": "🗺️ Регіони",
-        // Ігровий світ
-        "season01": "Сезон 01",
-        "setting": "✨ Сетинг",
-        "vessel": "🛸 Судна",
-    }
+  const nameMap: Record<string, string> = {
+    cataphract: "⚔️ Катафракти",
+    celestial: "🪐 Небесні тіла",
+    episodes: "🎬 Епізоди",
+    faction: "🚩 Фракції",
+    location: "📍 Місця",
+    npc: "🎭 НІПи",
+    pc: "🎭 ІПи",
+    player: "👤 Гравці",
+    region: "🗺️ Регіони",
+    // Ігровий світ
+    season01: "Сезон 01",
+    setting: "✨ Сетинг",
+    vessel: "🛸 Судна",
+  }
 
-    if (nameMap[folderName]) {
-        return nameMap[folderName]
-    }
-    return folderName
+  if (nameMap[folderName]) {
+    return nameMap[folderName]
+  }
+  return folderName
 }
